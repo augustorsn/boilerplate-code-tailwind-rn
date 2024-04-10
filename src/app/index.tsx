f2cdfa8ba8fa3c4945010ce232f7f06b83b5ea0e
guto <augustorsn@gmail.com>
2024-04-10 20:59:04 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+
+export default function Home() {     
+     
+    return (
+        <View  className="flex-1 bg-green-500 items-center justify-center">            
+            <Text className="text-white">Olá react native! </Text>            
+        </View>
+    )
+}
+
+
